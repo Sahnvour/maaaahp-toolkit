2ee1976746ad5f80643a4e4c90d119f3f7037aa3
Antoine Vugliano <antoine.vugliano@free.fr>
2013-10-22 17:09:24 +0200

    renommage

--- Aaaah/ui/EditorWindow.py
+++ Aaaah/ui/EditorWindow.py
@@ -2,11 +2,11 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from main import Ui_MainWindow
+from MainWindow import Ui_MainWindow
 
 class EditorWindow(QMainWindow):
 	def __init__(self, parent=None):
-		super(Editor, self).__init__(parent)
+		super(EditorWindow, self).__init__(parent)
 		self.setup()
 
 	def setup(self):
@@ -14,6 +14,6 @@ class EditorWindow(QMainWindow):
 		self.ui.setupUi(self)
 
 app = QApplication(sys.argv)
-editor = Editor()
+editor = EditorWindow()
 editor.show()
 sys.exit(app.exec_())
